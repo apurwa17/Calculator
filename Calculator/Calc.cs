ac9940e344a7136b7a06064d1f41f58f2eaf016b
apurwa17 <sharma.apurwakumar@gmail.com>
2018-03-23 15:37:36 +0530

    first commit

---
+++ Calculator/Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calc : Form
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            int addTotal = Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(addTotal);
+
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press return to launch the form.");
+            Console.ReadLine();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Calculator.Calc());
+        }
+    }
+}
